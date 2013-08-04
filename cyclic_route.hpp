42bf32a31db9c2bd6a0525af2b3461852e66c1df
jerry <jerrycheng567@gmail.com>
2013-08-04 16:21:08 +0800

    add struct iterator

--- cyclic_route.hpp
+++ cyclic_route.hpp
@@ -1,119 +1,89 @@
 #pragma once
+#include <algorithm>
 #include "coord.hpp"
 #include "direction.hpp"
 #include "path.hpp"
 #include "environment.hpp"
-
+#include <functional>
+#include "point/food.hpp"
 namespace snake
 {
+
+  struct cyclic_route;
+
+  struct cyclic_route_iterator : iterator< forward_iterator_tag, coord >
+  {
+    coord c;
+    const cyclic_route & cr;
+    cyclic_route_iterator( const cyclic_route &, coord && );
+    cyclic_route_iterator( const cyclic_route &, const coord & );
+    bool operator ==( const cyclic_route_iterator & );
+    bool operator !=( const cyclic_route_iterator & );
+    coord operator *( );
+    coord operator ++ ( );
+    coord operator ++ ( int );
+  };
+
   struct cyclic_route
   {
-    const env & e;
     vector< vector< pair< bool, direction > > > cr;
-    bool have( const coord & ) const;
-    direction operator [ ] ( const coord & ) const;
+    const env & e;
+    typedef cyclic_route_iterator iterator;
+
+    iterator begin( ) const;
     void clear( );
+    cyclic_route( const env & e ) : e( e ) { resize( e.cur_map_length, e.cur_map_width ); }
+    iterator end( ) const;
+    bool extend_thin_rect( const coord & c1, const coord c2 );
+    bool extend_rect( const coord & c1, const coord c2, const coord & c3, const coord & c4 );
+    direction get_dir( );
+    pair< bool, path > get_path( const coord & from, const coord & to, size_t min_path_size, size_t max_path_size, size_t time ) const;
+    pair< bool, path > get_path( const coord & from, const coord & to, size_t time ) const;
+    pair< bool, path > get_path( const coord & from, const coord & to ) const;
+    pair< bool, path > get_path( const coord & from, const coord & to, size_t min_path_size, size_t max_path_size, const set< coord > & occupied, size_t time ) const;    bool have( const coord & ) const;
+    bool need_update( ) const;
+    direction operator [ ] ( const coord & ) const;
     void resize( size_t length, size_t width );
     void update_route( const path & p );
-
-    cyclic_route( const env & e ) : e( e ) { resize( e.cur_map_length, e.cur_map_width ); }
     void update_route( );
-    bool extend_cyclic_route_thin_rect( const coord & c1, const coord c2 )
-    {
-      if ( c1.is_on_same_line( c2 ) )
-      {
-        direction c1_to_c2( c1.get_heading( c2 ) );
-        auto & c1_r = cr[ c1.y ][ c1.x ];
-        assert( c1_r.first == true );
-        if ( c1_r.second == c1_to_c2 || c1_r.second == c1_to_c2.reverse( ) )
-        {
-          direction t = c1_to_c2.tilt( ), t_r = c1_to_c2.tilt( ).reverse( );
-          if ( ( e.is_in_map_after_move( c1, t ) &&
-                 ( cr[ c1( t ).y ][ c1( t ).x ].first == true ) &&
-                 ( cr[ c1( t ).y ][ c1( t ).x ].second == t_r ) &&
-                 extend_rect( c1( t ), c2( t ), c2, c1 ) ) ||
-               ( e.is_in_map_after_move( c1, t_r ) &&
-                 ( cr[ c1( t_r ).y ][ c1( t_r ).x ].first == true ) &&
-                 ( cr[ c1( t_r ).y ][ c1( t_r ).x ].second == t ) &&
-                 extend_rect( c1( t_r ), c2( t_r ), c2, c1 ) )
-               )
-          {
-            return true;
-          }
-          else
-          {
-            return false;
-          }
-        }
-        else
-        {
-          return extend_rect( c1, c2, c2( c1_r.second ), c1( c1_r.second ) );
-        }
-      }
-      else
-      {
-        return extend_rect( c1, coord( c1.y, c2.x ), c2, coord( c2.y, c1.x ) ) || extend_rect( c1, coord( c2.y, c1.x ), c2, coord( c1.y, c2.x ) );
-      }
-    }
 
-    direction get_dir( );
-
-    bool extend_rect( const coord & c1, const coord c2, const coord & c3, const coord & c4 )
-    {
-      assert( c1.is_on_same_line( c2 ) );
-      assert( c2.is_on_same_line( c3 ) );
-      assert( c3.is_on_same_line( c4 ) );
-      assert( c4.is_on_same_line( c1 ) );
-      assert( ! c1.is_on_same_line( c3 ) );
-      assert( ! c2.is_on_same_line( c4 ) );
-      assert( have( c1 ) == true );
-      assert( have( c2 ) == false );
-      assert( have( c3 ) == false );
-      assert( have( c4 ) == true );
-      cr[ c1.y ][ c1.x ].first = false;
-      cr[ c4.y ][ c4.x ].first = false;
-      auto dc12( coord::distance( c1, c2 ) ), dc23( coord::distance( c2, c3 ) ), dc34( coord::distance( c3, c4 ) );
-      pair< bool, path > c1_to_c2( get_path( c1, c2, 0 ) );
-      pair< bool, path > c2_to_c3( get_path( c2, c3, dc12 ) );
-      pair< bool, path > c3_to_c4( get_path( c3, c4, dc12 + dc23 ) );
-      pair< bool, path > c4_to_c1( get_path( c4, c1, dc12 + dc23 + dc34 ) );
-      if( c1_to_c2.first &&
-          c2_to_c3.first &&
-          c3_to_c4.first &&
-          c4_to_c1.first )
-      {
-        update_route( c1_to_c2.second );
-        update_route( c2_to_c3.second );
-        update_route( c3_to_c4.second );
-        update_route( c4_to_c1.second );
-        return true;
-      }
-      cr[ c1.y ][ c1.x ].first = true;
-      cr[ c4.y ][ c4.x ].first = true;
-      return false;
-    }
-
-
-    bool need_update( ) const;
-    pair< bool,  path > get_path( const coord & from, const coord & to, size_t min_path_size, size_t max_path_size, size_t time ) const
-    {
-      return get_path( from, to, min_path_size, max_path_size, set< coord >( ), time );
-    }
-
-    pair< bool, path > get_path( const coord & from, const coord & to, size_t time ) const
-    {
-      auto dis = coord::distance( from, to );
-      return get_path( from, to, dis, dis, time );
-    }
-
-    pair< bool, path > get_path( const coord & from, const coord & to ) const
-    {
-      auto dis = coord::distance( from, to );
-      return get_path( from, to, dis, dis, 0 );
-    }
-
-    pair< bool, path > get_path( const coord & from, const coord & to, size_t min_path_size, size_t max_path_size, const set< coord > & occupied, size_t time ) const;
   };
+
+  coord cyclic_route_iterator::operator ++ ( int )
+  {
+    coord c( * * this );
+    ++( * this );
+    return c;
+  }
+
+  coord cyclic_route_iterator::operator ++ ( )
+  {
+    c = c( cr[ c ] );
+    return * * this;
+  }
+
+  coord cyclic_route_iterator::operator * ( )
+  {
+    return c;
+  }
+
+  cyclic_route_iterator::cyclic_route_iterator ( const cyclic_route & cr, const coord & c ) : c( c ), cr( cr ) { }
+
+  bool cyclic_route_iterator::operator == ( const cyclic_route_iterator & i )
+  {
+    assert( ( & cr ) == ( & i.cr ) );
+    return c == i.c;
+  }
+
+  bool cyclic_route_iterator::operator != ( const cyclic_route_iterator & i )
+  {
+    assert( ( & cr ) == ( & i.cr ) );
+    return c != i.c;
+  }
+
+}
+namespace snake
+{
 
   bool cyclic_route::have( const coord & c ) const
   {
@@ -132,9 +102,16 @@ namespace snake
     cr.resize( length, vector< pair< bool, direction > >( width ) );
   }
 
-}
-namespace snake
-{
+  cyclic_route::iterator cyclic_route::begin( ) const
+  {
+    return iterator( * this, e.cur_head );
+  }
+
+  cyclic_route::iterator cyclic_route::end( ) const
+  {
+    return iterator( * this, e.cur_head );
+  }
+
   void cyclic_route::update_route( const path & p )
   {
     coord c = p.head;
@@ -189,12 +166,50 @@ namespace snake
       }
       if ( closest_coord.is_on_same_line( f ) )
       {
-        if( ! extend_cyclic_route_thin_rect( closest_coord, f ) ) { assert( false ); }
+        if( ! extend_thin_rect( closest_coord, f ) ) { assert( false ); }
       }
       else
       {
         assert( false );
       }
+    }
+  }
+
+  bool cyclic_route::extend_thin_rect(const coord & c1, const coord c2)
+  {
+    if ( c1.is_on_same_line( c2 ) )
+    {
+      direction c1_to_c2( c1.get_heading( c2 ) );
+      auto & c1_r = cr[ c1.y ][ c1.x ];
+      assert( c1_r.first == true );
+      if ( c1_r.second == c1_to_c2 || c1_r.second == c1_to_c2.reverse( ) )
+      {
+        direction t = c1_to_c2.tilt( ), t_r = c1_to_c2.tilt( ).reverse( );
+        if ( ( e.is_in_map_after_move( c1, t ) &&
+               ( cr[ c1( t ).y ][ c1( t ).x ].first == true ) &&
+               ( cr[ c1( t ).y ][ c1( t ).x ].second == t_r ) &&
+               extend_rect( c1( t ), c2( t ), c2, c1 ) ) ||
+             ( e.is_in_map_after_move( c1, t_r ) &&
+               ( cr[ c1( t_r ).y ][ c1( t_r ).x ].first == true ) &&
+               ( cr[ c1( t_r ).y ][ c1( t_r ).x ].second == t ) &&
+               extend_rect( c1( t_r ), c2( t_r ), c2, c1 ) )
+             )
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+      }
+      else
+      {
+        return extend_rect( c1, c2, c2( c1_r.second ), c1( c1_r.second ) );
+      }
+    }
+    else
+    {
+      return extend_rect( c1, coord( c1.y, c2.x ), c2, coord( c2.y, c1.x ) ) || extend_rect( c1, coord( c2.y, c1.x ), c2, coord( c1.y, c2.x ) );
     }
   }
 
@@ -206,8 +221,49 @@ namespace snake
     return ( * this )[ e.cur_head ];
   }
 
+  bool cyclic_route::extend_rect(const coord & c1, const coord c2, const coord & c3, const coord & c4)
+  {
+    assert( c1.is_on_same_line( c2 ) );
+    assert( c2.is_on_same_line( c3 ) );
+    assert( c3.is_on_same_line( c4 ) );
+    assert( c4.is_on_same_line( c1 ) );
+    assert( ! c1.is_on_same_line( c3 ) );
+    assert( ! c2.is_on_same_line( c4 ) );
+    assert( have( c1 ) == true );
+    assert( have( c2 ) == false );
+    assert( have( c3 ) == false );
+    assert( have( c4 ) == true );
+    cr[ c1.y ][ c1.x ].first = false;
+    cr[ c4.y ][ c4.x ].first = false;
+    auto dc12( coord::distance( c1, c2 ) ), dc23( coord::distance( c2, c3 ) ), dc34( coord::distance( c3, c4 ) );
+    pair< bool, path > c1_to_c2( get_path( c1, c2, 0 ) );
+    pair< bool, path > c2_to_c3( get_path( c2, c3, dc12 ) );
+    pair< bool, path > c3_to_c4( get_path( c3, c4, dc12 + dc23 ) );
+    pair< bool, path > c4_to_c1( get_path( c4, c1, dc12 + dc23 + dc34 ) );
+    if( c1_to_c2.first &&
+        c2_to_c3.first &&
+        c3_to_c4.first &&
+        c4_to_c1.first )
+    {
+      update_route( c1_to_c2.second );
+      update_route( c2_to_c3.second );
+      update_route( c3_to_c4.second );
+      update_route( c4_to_c1.second );
+      return true;
+    }
+    cr[ c1.y ][ c1.x ].first = true;
+    cr[ c4.y ][ c4.x ].first = true;
+    return false;
+  }
+
+  bool is_food( const env & e, const coord & c )
+  {
+    return dynamic_cast< food * >( e.vec[ c.y ][ c.x ].get( ) ) != nullptr;
+  }
+
   bool cyclic_route::need_update( ) const
   {
+    return any_of( begin( ), end( ), function< bool ( const coord & ) >( bind( is_food, e, placeholders::_1 ) ) );
     if ( ! have( e.cur_head ) )
     {
       return true;
@@ -224,7 +280,25 @@ namespace snake
     }
     return true;
   }
-  pair<bool, path> cyclic_route::get_path(const coord & from, const coord & to, size_t min_path_size, size_t max_path_size, const set<coord> & occupied, size_t time) const
+
+  pair< bool, path > cyclic_route::get_path(const coord & from, const coord & to, size_t min_path_size, size_t max_path_size, size_t time) const
+  {
+    return get_path( from, to, min_path_size, max_path_size, set< coord >( ), time );
+  }
+
+  pair< bool, path > cyclic_route::get_path(const coord & from, const coord & to, size_t time) const
+  {
+      auto dis = coord::distance( from, to );
+      return get_path( from, to, dis, dis, time );
+  }
+
+  pair< bool, path > cyclic_route::get_path( const coord & from, const coord & to ) const
+  {
+    auto dis = coord::distance( from, to );
+    return get_path( from, to, dis, dis, 0 );
+  }
+
+  pair< bool, path > cyclic_route::get_path(const coord & from, const coord & to, size_t min_path_size, size_t max_path_size, const set<coord> & occupied, size_t time) const
   {
     {
       if ( from == to && min_path_size == 0 )
--- point/food.hpp
+++ point/food.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "point.hpp"
 #include "snake_piece.hpp"
 namespace snake
